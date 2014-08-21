430340c60e7dd0cd02824a56daddc8d5e9e747d6
andrewxhill <andrewxhill@gmail.com>
2014-08-21 13:20:20 -0400

    recast on the fly

---
+++ geocoder/ip-addresses/sql/build_data_table.sql
@@ -0,0 +1,9 @@
+
+---- Postal Code Polygon table ---
+--- ---
+
+-- Clear table
+
+DELETE FROM ip_address_locations;
+INSERT INTO ip_address_locations (the_geom, network_start_ip) SELECT the_geom, network_start_ip::inet FROM latest_ip_address_locations;
+DROP TABLE latest_ip_address_locations;
