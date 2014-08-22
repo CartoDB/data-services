e0ee93bc3e5b66750110c55952d45b0392af5ea4
andrewxhill <andrewxhill@gmail.com>
2014-08-22 13:21:14 -0400

    removed single array method of the admin1 polygon geocoder

--- geocoder/setup/indexes.sql
+++ geocoder/setup/indexes.sql
@@ -7,3 +7,5 @@ CREATE INDEX idx_admin0_synonyms_rank ON admin0_synonyms (rank);
 -- create indexes on polygon table
 CREATE UNIQUE INDEX idx_ne_admin0_v3_adm0_a3 ON ne_admin0_v3 (adm0_a3);
 
+-- create indexes on postal code polygon table
+CREATE UNIQUE INDEX idx_postal_code_polygons_a3_code ON postal_code_polygons (adm0_a3, postal_code)
