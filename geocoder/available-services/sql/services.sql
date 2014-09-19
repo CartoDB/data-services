cf609dbac9b6c73477e0006910840332e1f2f896
andrewxhill <andrewxhill@gmail.com>
2014-09-20 00:05:53 +0200

    first draft available services api

--- geocoder/available-services/sql/services.sql
+++ geocoder/available-services/sql/services.sql
@@ -1,12 +1,12 @@
---CREATE TYPE geocode_admin_v1 AS (q TEXT, geom GEOMETRY, success BOOLEAN);
+--CREATE TYPE available_services_v1 AS (q TEXT, adm0_a3 TEXT, postal_code_points BOOLEAN, postal_code_polygons BOOLEAN);
 
-CREATE OR REPLACE FUNCTION geocode_admin0_polygons(name text[])
-  RETURNS SETOF geocode_admin_v1 AS $$
+CREATE OR REPLACE FUNCTION admin0_available_services(countries text[])
+  RETURNS SETOF available_services_v1 AS $$
   DECLARE 
-    ret geocode_admin_v1%rowtype;
+    ret available_services_v1%rowtype;
   BEGIN
   -- FOR ret IN
   RETURN QUERY
-    SELECT d.q, n.the_geom as geom, CASE WHEN s.adm0_a3 IS NULL then FALSE ELSE TRUE END AS success FROM (SELECT q, lower(regexp_replace(q, '[^a-zA-Z\u00C0-\u00ff]+', '', 'g'))::text x FROM (SELECT unnest(name) q) g) d LEFT OUTER JOIN admin0_synonyms s ON name_ = d.x LEFT OUTER JOIN ne_admin0_v3 n ON s.adm0_a3 = n.adm0_a3 GROUP BY d.q, n.the_geom, s.adm0_a3;
+    SELECT d.q, n.adm0_a3, n.postal_code_points, n.postal_code_polygons FROM (SELECT q, lower(regexp_replace(q, '[^a-zA-Z\u00C0-\u00ff]+', '', 'g'))::text x FROM (SELECT unnest(name) q) g) d LEFT OUTER JOIN admin0_synonyms s ON name_ = d.x LEFT OUTER JOIN available_services n ON s.adm0_a3 = n.adm0_a3 GROUP BY d.q, n.adm0_a3, n.postal_code_points, n.postal_code_polygons;
 END
 $$ LANGUAGE 'plpgsql' SECURITY DEFINER;
