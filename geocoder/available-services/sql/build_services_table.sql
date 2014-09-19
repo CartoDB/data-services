d42e72bad6a43b2a7e883fcec1e8cc4b93b1515f
andrewxhill <andrewxhill@gmail.com>
2014-09-19 23:57:43 +0200

    point postal code service check

--- geocoder/available-services/sql/build_services_table.sql
+++ geocoder/available-services/sql/build_services_table.sql
@@ -8,12 +8,12 @@ UPDATE available_services SET admin0 = TRUE WHERE 0 < (SELECT count(*) FROM glob
 
 
 -- Postalcode polygons
-UPDATE available_services SET postalcode_polygons = FALSE;
-UPDATE available_services SET postalcode_polygons = TRUE WHERE 0 < (SELECT count(*) FROM postal_code_polygons WHERE available_services.adm0_a3 = adm0_a3 LIMIT 1);
+UPDATE available_services SET postal_code_polygons = FALSE;
+UPDATE available_services SET postal_code_polygons = TRUE WHERE 0 < (SELECT count(*) FROM postal_code_polygons WHERE available_services.adm0_a3 = adm0_a3 LIMIT 1);
 
 
 -- Postalcode points
-UPDATE available_services SET postalcode_points = FALSE;
-UPDATE available_services SET postalcode_points = TRUE WHERE 0 < (SELECT count(*) FROM zipcode_points WHERE available_services.adm0_a3 = iso3 LIMIT 1);
+UPDATE available_services SET postal_code_points = FALSE;
+UPDATE available_services SET postal_code_points = TRUE WHERE 0 < (SELECT count(*) FROM postal_code_points WHERE available_services.adm0_a3 = adm0_a3 LIMIT 1);
 
 
