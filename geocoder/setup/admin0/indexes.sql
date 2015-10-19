6a7e63d2751d9cca96e2d136f30d349319c04b98
Carla Iriberri <iriberrig@gmail.com>
2015-10-19 14:46:31 +0200

    Small edits

--- geocoder/setup/admin0/indexes.sql
+++ geocoder/setup/admin0/indexes.sql
@@ -1,4 +1,4 @@
--- create indexes on synonyms table
+-- Create indexes on synonyms table
 CREATE INDEX idx_admin0_synonyms_name_patt ON admin0_synonyms (name_ text_pattern_ops);
 --CREATE INDEX idx_admin0_synonyms_name ON admin0_synonyms (lower(regexp_replace(name, '\W+'::text, ''::text)));
 CREATE INDEX idx_admin0_synonyms_name_ ON admin0_synonyms (name_);
@@ -6,5 +6,5 @@ CREATE INDEX idx_admin0_synonyms_name_ ON admin0_synonyms (name_);
 CREATE INDEX idx_admin0_synonyms_rank ON admin0_synonyms (rank);
 CREATE INDEX idx_admin0_synonyms_name_rank ON admin0_synonyms (name_, rank);
 
--- create indexes on polygon table
+-- Create indexes on polygon table
 CREATE UNIQUE INDEX idx_ne_admin0_v3_adm0_a3 ON ne_admin0_v3 (adm0_a3);
