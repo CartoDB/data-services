fcd7521b26b63ebf72b9d7a02e00f492b9b36870
Carla Iriberri <iriberrig@gmail.com>
2015-11-03 15:37:35 +0100

    Fixes tests and out comparison file for IP addresses

--- geocoder/ip-addresses/extension/sql/cdb_geocoder_ipaddr_test.sql
+++ geocoder/ip-addresses/extension/sql/cdb_geocoder_ipaddr_test.sql
@@ -16,13 +16,9 @@ $$
 LANGUAGE plpgsql;
 
 -- Add a few IP sources
-COPY ip_address_locations (network_start_ip, the_geom, cartodb_id, created_at, updated_at, the_geom_webmercator) FROM stdin;
-::ffff:2.235.35.0	0101000020E610000072F90FE9B7CF22405DFE43FAEDC34640	2821226	2014-08-25 10:35:51.665546+00	2014-08-25 10:35:51.665546+00	0101000020110F000010801778FBF32F4109868FF8BCC35541
-::ffff:31.7.187.0       \N      2783250 2014-08-25 10:35:51.665546+00   2014-08-25 10:35:51.665546+00   \N
-::ffff:64.110.146.0     \N      2783251 2014-08-25 10:35:51.665546+00   2014-08-25 10:35:51.665546+00   \N
-::ffff:72.5.198.0       \N      2783252 2014-08-25 10:35:51.665546+00   2014-08-25 10:35:51.665546+00   \N
-::ffff:77.73.184.0      \N      2783253 2014-08-25 10:35:51.665546+00   2014-08-25 10:35:51.665546+00   \N
-\.
+INSERT INTO ip_address_locations (network_start_ip, the_geom) VALUES ('::ffff:2.235.35.0'::inet, '0101000020E610000072F90FE9B7CF22405DFE43FAEDC34640');
+INSERT INTO ip_address_locations (network_start_ip, the_geom) VALUES ('::ffff:31.7.187.0'::inet, '');
+INSERT INTO ip_address_locations (network_start_ip, the_geom) VALUES ('::ffff:64.110.146.0'::inet, '');
 
 -- Check that the geocoding function is callable, should return success = true
-SELECT (geocode_ip(Array['2.235.35.0'])).*;
+SELECT (geocode_ip(Array['2.235.35.0'])).geom;
