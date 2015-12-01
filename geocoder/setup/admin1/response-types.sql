c3924fb41d8bba76d759b8108e6dd4d1c2c871c1
Carla <carla@cartodb.com>
2015-12-01 15:19:16 +0100

    Update response-types.sql

--- geocoder/setup/admin1/response-types.sql
+++ geocoder/setup/admin1/response-types.sql
@@ -1,15 +1,3 @@
 -- Response types for admin1 geocoder
-
-CREATE TYPE geocode_admin_v1 AS (
-    q text,
-    geom geometry,
-    success boolean
-);
-
-
-CREATE TYPE geocode_admin_country_v1 AS (
-    q text,
-    c text,
-    geom geometry,
-    success boolean
-);
+CREATE TYPE geocode_admin_v1 AS (q TEXT, geom GEOMETRY, success BOOLEAN);
+CREATE TYPE geocode_admin_country_v1 AS (q TEXT, c TEXT, geom GEOMETRY, success BOOLEAN);
