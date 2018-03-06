a6c118ceb0c8e84cd2312a48499e3c57069cde4c
Alex Martín <amartin@carto.com>
2018-03-06 18:09:45 +0100

    Prevent from updating more than once

--- geocoder/admin0/patches/20180306_add_ssd_rows_for_south_sudan.sql
+++ geocoder/admin0/patches/20180306_add_ssd_rows_for_south_sudan.sql
@@ -1,1 +1,8 @@
-insert into admin0_synonyms (name, rank, adm0_a3, name_) select name, rank, 'SSD' as adm0_a3, name_ from admin0_synonyms where adm0_a3 = 'SDS';
+DO $$
+  DECLARE updated INTEGER;
+BEGIN
+  SELECT count(*) FROM admin0_synonyms WHERE adm0_a3 = 'SSD' INTO updated
+  IF updated = 0 THEN
+    INSERT INTO admin0_synonyms (name, rank, adm0_a3, name_) SELECT name, rank, 'SSD' AS adm0_a3, name_ FROM admin0_synonyms WHERE adm0_a3 = 'SDS';
+  END IF;
+END
