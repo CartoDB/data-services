a468a01d9ca0fe7e751b58d7ffd0bc980e006085
Mario de Frutos <ethervoid@gmail.com>
2018-03-06 18:46:57 +0100

    Make the patch ideompotent

--- geocoder/admin0/patches/20160622_countries_synonym_congo.sql
+++ geocoder/admin0/patches/20160622_countries_synonym_congo.sql
@@ -1,3 +1,9 @@
 -- Patch 0.0.1 data version -20160622
-
-INSERT INTO admin0_synonyms (name, rank, adm0_a3) VALUES ('Republic of the Congo', 10, 'COG');
+DO $$
+    DECLARE exiting INTEGER;
+BEGIN
+    SELECT count(*) FROM admin0_synonyms WHERE name = 'Republic of the Congo' AND adm0_a3 = 'COG' and rank = 10 INTO exiting;
+    IF exiting = 0 THEN
+        INSERT INTO admin0_synonyms (name, rank, adm0_a3) VALUES ('Republic of the Congo', 10, 'COG');
+    END IF;
+END$$;
